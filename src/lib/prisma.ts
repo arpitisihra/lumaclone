0bba9e2cc39f50d5c12364c0eb63262489892276
arpitisihra <arpit.sihra@gmail.com>
2025-09-02 17:50:03 +0530

    Feat: Add image upload API and clean up

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,20 @@
 // src/lib/prisma.ts
-// This file provides a singleton instance of the Prisma Client.
-
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  // @ts-ignore
-  if (!global.prisma) {
-    // @ts-ignore
-    global.prisma = new PrismaClient();
-  }
-  // @ts-ignore
-  prisma = global.prisma;
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
